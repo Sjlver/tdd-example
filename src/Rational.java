99954de5e94a4df1de720b52f015165cc6d482ff
Jonas Wagner <jonas.wagner@epfl.ch>
2013-10-07 08:45:59 +0200

    Creating a rational number.

---
+++ src/Rational.java
@@ -0,0 +1,8 @@
+
+public class Rational {
+
+	public Rational(int n, int d) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/RationalTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+
+public class RationalTest {
+
+	@Test
+	public void testCreateRational() {
+		new Rational(4, 3);
+	}
+
+}
